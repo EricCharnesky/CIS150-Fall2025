f96b3c8cb9bdccf7493f92f9f4f834d288e5d131
Eric Charnesky <EricCharnesky@gmail.com>
2025-09-08 12:04:50 -0400

    week 3 examples

--- Week3/Week3/Week3.cpp
+++ Week3/Week3/Week3.cpp
@@ -1,20 +1,138 @@
-// Week3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!" << endl;
+    cout << "How much money do you have in your pocket?" << endl;
+
+    double moneyInMyPocket;
+    cin >> moneyInMyPocket;
+
+    if (moneyInMyPocket > 15) {
+        cout << "You can get a combo at Picasso" << endl;
+    }
+    // this can never run, the first if will run instead
+    else if (moneyInMyPocket > 20) {
+        cout << "You're buying lunch for your friend as well" << endl;
+    }
+    else if (moneyInMyPocket > 10) {
+        cout << "You can get lunch at Picasso" << endl;
+        cout << "More lines of code" << endl;
+    }
+    else {
+        cout << "You get ramen noodles" << endl;
+    }
+
+    cout << "Do you want a pop? ( y/n )" << endl;
+    // string input
+    char input;
+    cin >> input;
+
+    // y or Y - can use strings in if statements
+    //if (input == "y" || input == "Y" )
+    if (input == 'y' || input == 'Y')
+    {
+
+    }
+
+    // switches don't work with strings, must be char only
+    switch (input) {
+    case 'y':
+    case 'Y':
+        cout << "Enter a choice" << endl;
+        cout << "1 - Coke" << endl;
+        cout << "2 - Coke Zero" << endl;
+        cout << "3 - Mountain Dew" << endl;
+        cout << "4 - Sprite" << endl;
+
+        int choice;
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            cout << "You got a coke with your lunch" << endl;
+            break; // please stop the switch
+        case 2:
+            cout << "You got a coke zero with your lunch" << endl;
+            break;
+        case 3:
+            cout << "Do the dew!" << endl;
+            break;
+        case 4:
+            cout << "Sprite for you" << endl;
+            break;
+        default:
+            cout << "Invalid choice, no pop for you" << endl;
+            // no break needed, it's already done
+        }
+
+        if (choice == 1) {
+            cout << "Coke" << endl;
+        }
+        else if (choice == 2) {
+            cout << "Coke zero" << endl;
+        }
+        else if (choice == 3) {
+            cout << "Do the dew!" << endl;
+        }
+        else if (choice == 4) {
+            cout << "sprite" << endl;
+        }
+        else {
+            cout << "invalid" << endl;
+        }
+
+        break;
+    }
+
+    int assignment1Score, assignment2Score;
+    int assignment3Score;
+    int assignment4Score;
+
+    bool amHungry = true;
+
+    // if ( amHungry == true ) - redundant
+    if (amHungry) {
+        cout << "Go eat!" << endl;
+    }
+
+    if (amHungry != false) {
+
+    }
+    
+    bool notHungry = false;
+
+    // ( notHungry == false )
+    if (!notHungry) {
+        cout << "go eat!" << endl;
+    }
+
+    // arithmetic happens first, then relational
+    if (10 + 4 > 15) {
+
+
+    }
+
+
+    if (10 + 4 > 15 && 5 < 20 || 20 > 7) {
+        cout << "yes" << endl;
+    }
+
+    // true && true == true
+    // true && false == false
+    // false && true == false
+    // false && false == false
+
+    // true || true == true
+    // true || false == true
+    // false || true == ture
+    // false || false == false
+
+
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
