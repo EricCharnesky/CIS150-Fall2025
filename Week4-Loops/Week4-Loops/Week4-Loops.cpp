887aefca5f2140e4773919cc18e51ae0a7bd8b20
Eric Charnesky <EricCharnesky@gmail.com>
2025-09-17 12:31:38 -0400

    triangles

--- Week4-Loops/Week4-Loops/Week4-Loops.cpp
+++ Week4-Loops/Week4-Loops/Week4-Loops.cpp
@@ -8,6 +8,105 @@ int main()
 {
     // bing "c++ srand time"
     srand(time(0));
+
+    int rows;
+    cout << "How many rows do you want?" << endl;
+    cin >> rows;
+
+    int columns;
+    cout << "How many columns do you want?" << endl;
+    cin >> columns;
+
+    for (int row = 1; row <= rows; row++) {
+        for (int column = 1; column <= columns; column++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    cout << endl << endl;
+
+    int height;
+    cout << "Enter the height of a triangle" << endl;
+    cin >> height;
+
+    for (int row = 1; row <= height; row++) {
+        for (int column = 1; column <= row; column++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    cout << endl << endl;
+
+    int base;
+    cout << "How many stars in your base" << endl;
+    cin >> base;
+
+    while (base % 2 == 0) {
+        cout << "Must be odd ( like Eric )" << endl;
+        cout << "How many stars in your base" << endl;
+        cin >> base;
+    }
+
+    rows = base / 2 + 1;
+    int spaces = rows - 1;
+    int stars = 1;
+
+    for (int row = 1; row <= rows; row++) {
+        for (int space = 1; space <= spaces; space++) {
+            cout << " ";
+        }
+        for (int star = 1; star <= stars; star++) {
+            cout << "*";
+        }
+        cout << endl;
+        spaces--;
+        stars += 2;
+        // stars = stars + 2;
+    }
+
+    cout << endl << endl;
+
+    // row 1 - spaces 4 - 1 stars
+    // row 2 - spaces 3 - 3 stars
+    // row 3 - spaces 2 - 5 stars
+    // row 4 - spaces 1 - 7 stars
+    // row 5 - spaces 0 - 9 stars
+
+    cout << "Let's print 0-9 with a for loop" << endl;
+    //                       while
+    // initialize a value, test a value, increment a value
+    for (int number = 0; number < 10; number++) {
+        cout << number << endl;
+    }
+
+    cout << "Let's print 0-9 with a while loop" << endl;
+    int number = 0;
+    while (number < 10) {
+        cout << number << endl;
+        number++;
+    }
+
+    cout << "Let's print 0-9 with a do loop" << endl;
+    int doNumber = 0;
+    do {
+        cout << doNumber << endl;
+        doNumber++;
+    } while (doNumber < 10);
+
+
+
+    char more;
+
+    // post test loop - runs AT LEAST once
+    do {
+        cout << "Do you want more? (y/n)" << endl;
+        cin >> more;
+    } while (more != 'y' && more != 'n');
+
+
+
+    
     char playAgain = 'y';
 
     while (playAgain == 'y') {
@@ -49,6 +148,7 @@ int main()
         cout << "Do you want to play again? (y/n)" << endl;
         cin >> playAgain;
 
+        // validation loop for characters are not a valid option
         while (playAgain != 'y' && playAgain != 'n') {
             cout << "Enter only y or n" << endl;
             cout << "Do you want to play again ? (y / n)" << endl;
