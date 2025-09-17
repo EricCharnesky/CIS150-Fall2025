f1b16f3e64267fc6922d698e8beff90df88beb6c
Eric Charnesky <EricCharnesky@gmail.com>
2025-09-17 11:23:05 -0400

    guess a number game

--- Week4-Loops/Week4-Loops/Week4-Loops.cpp
+++ Week4-Loops/Week4-Loops/Week4-Loops.cpp
@@ -1,11 +1,35 @@
 
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    // bing "c++ srand time"
+    srand(time(0));
+    int luckyNumber = rand() % 100 + 1;
+
+    cout << "Guess a number 1 - 100: " << endl;
+
+    int guess;
+    cin >> guess;
+
+    // runs if the condition is true
+    while (guess != luckyNumber) {
+        if (guess < luckyNumber) {
+            cout << "too low!" << endl;
+        }
+        else {
+            cout << "too high!" << endl;
+        }
+        cout << "Guess again: " << endl;
+        cin >> guess;
+    } // go back and check the condition
+
+
+    cout << "You guessed it!" << endl;
+
 
 
 }
