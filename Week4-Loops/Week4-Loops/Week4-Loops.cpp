9a6254f38350173ff2bf4bc13a8f25cfd14d6230
Eric Charnesky <EricCharnesky@gmail.com>
2025-09-17 11:35:25 -0400

    allows for setting the max number & play again

--- Week4-Loops/Week4-Loops/Week4-Loops.cpp
+++ Week4-Loops/Week4-Loops/Week4-Loops.cpp
@@ -6,16 +6,27 @@ using namespace std;
 
 int main()
 {
+    // bing "c++ srand time"
     srand(time(0));
     char playAgain = 'y';
 
     while (playAgain == 'y') {
 
-        // bing "c++ srand time"
-        
-        int luckyNumber = rand() % 10 + 1;
+        cout << "How high of a number do you want to guess?" << endl;
+        int maxNumber;
+        cin >> maxNumber;
 
-        cout << "Guess a number 1 - 10: " << endl;
+        // invalid range check for validate loop
+        while (maxNumber <= 0 || maxNumber > 1000000) {
+            cout << "Must be greater than 0 and less than 1,000,000" << endl;
+            cout << "How high of a number do you want to guess?" << endl;
+            cin >> maxNumber;
+        }
+
+        
+        int luckyNumber = rand() % maxNumber + 1;
+
+        cout << "Guess a number 1 - " << maxNumber << ":" << endl;
 
         int guess;
         cin >> guess;
@@ -37,5 +48,11 @@ int main()
 
         cout << "Do you want to play again? (y/n)" << endl;
         cin >> playAgain;
+
+        while (playAgain != 'y' && playAgain != 'n') {
+            cout << "Enter only y or n" << endl;
+            cout << "Do you want to play again ? (y / n)" << endl;
+            cin >> playAgain;
+        }
     }
 }
