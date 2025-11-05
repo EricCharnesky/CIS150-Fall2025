0ddffb0f707fdeb2b4b04deaf3ac08df561197ef
Eric Charnesky <2469199+EricCharnesky@users.noreply.github.com>
2025-11-05 12:15:31 -0500

    arrays and vectors part 3

--- Week10-ArraysAndVectors/Week10-ArraysAndVectors/Week10-ArraysAndVectors.cpp
+++ Week10-ArraysAndVectors/Week10-ArraysAndVectors/Week10-ArraysAndVectors.cpp
@@ -30,8 +30,158 @@ int sumOfArray(int numbers[], int size) {
     return sum;
 }
 
+int maxValue(vector<int> numbers) {
+    int largest = numbers.at(0);
+
+    for (int value : numbers) {
+        if (value > largest ) {
+            largest = value;
+        }
+    }
+
+    return largest;
+}
+
+double average(vector<int> numbers) {
+    double total = 0;
+    for (int value : numbers) {
+        total += value;
+    }
+    return total / numbers.size();
+}
+
+void selectionSort(vector<int>& numbers) {
+    for (int indexToPutBiggest = numbers.size() - 1; indexToPutBiggest > 0; indexToPutBiggest--) {
+        int indexOfBiggest = 0;
+        for (int indexToCompare = 0; indexToCompare <= indexToPutBiggest; indexToCompare++) {
+            if (numbers.at(indexToCompare) > numbers.at(indexOfBiggest)) {
+                indexOfBiggest = indexToCompare;
+            }
+        }
+        int biggest = numbers.at(indexOfBiggest);
+        numbers.at(indexOfBiggest) = numbers.at(indexToPutBiggest);
+        numbers.at(indexToPutBiggest) = biggest;
+    }
+}
+
+void insertionSort(vector<int>& numbers) {
+    for (int indexToSort = 1; indexToSort < numbers.size(); indexToSort++) {
+        for (int indexToSwap = indexToSort; indexToSwap > 0; indexToSwap--) {
+            if (numbers.at(indexToSwap) < numbers.at(indexToSwap - 1)) {
+                int temp = numbers.at(indexToSwap);
+                numbers.at(indexToSwap) = numbers.at(indexToSwap - 1);
+                numbers.at(indexToSwap - 1) = temp;
+            }
+            else {
+                break;
+            }
+        }
+    }
+}
+
+void printVectorOnOneLine(vector<int> numbers) {
+    for (int value : numbers) {
+        cout << value << " ";
+    }
+    cout << endl;
+}
+
+// doesn't check for winner
+bool isTied(vector<vector<char>> board) {
+    for (vector<char> row : board) {
+        for (char column : row) {
+            if (column == ' ') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool hasWinnerByRow(vector<vector<char>> board) {
+    for (vector<char> row : board) {
+        if (row.at(0) != ' ' && row.at(0) == row.at(1) && row.at(0) == row.at(2)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool hasWinnerByColumn(vector<vector<char>> board) {
+    for (int columnIndex = 0; columnIndex < board.size(); columnIndex++) {
+        if (board.at(0).at(columnIndex) != ' ' &&
+            board.at(0).at(columnIndex) == board.at(1).at(columnIndex) &&
+            board.at(0).at(columnIndex) == board.at(2).at(columnIndex)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool hasWinnerByDiagonal(vector<vector<char>> board) {
+    return (board.at(1).at(1) != ' ' &&
+        ((board.at(0).at(0) == board.at(1).at(1) && board.at(0).at(0) == board.at(2).at(2))
+      || (board.at(0).at(2) == board.at(1).at(1) && board.at(0).at(2) == board.at(2).at(0))));
+}
+
+bool hasWinner(vector<vector<char>> board) {
+    return hasWinnerByRow(board) || hasWinnerByColumn(board) || hasWinnerByDiagonal(board);
+}
+
+bool gameOver(vector<vector<char>> board) {
+    return isTied(board) || hasWinner(board);
+}
+
+void printBoard(vector<vector<char>> board) {
+    cout << board.at(0).at(0) << "|" << board.at(0).at(1) << "|" << board.at(0).at(2) << endl;
+    cout << "-----" << endl;
+    cout << board.at(1).at(0) << "|" << board.at(1).at(1) << "|" << board.at(1).at(2) << endl;
+    cout << "-----" << endl;
+    cout << board.at(2).at(0) << "|" << board.at(2).at(1) << "|" << board.at(2).at(2) << endl;
+}
+
+void markBoard(char currentPlayer, vector<vector<char>>& board) {
+    int row;
+    int column;
+    
+    cout << currentPlayer << "'s turn" << endl;
+    cout << "Enter a row and column to mark" << endl;
+    cin >> row >> column;
+    while (row < 0 || row > 2 || column < 0 || column > 2 || board.at(row).at(column) != ' ') {
+        cout << "Invalid spot!" << endl;
+        cout << "Enter a row and column to mark" << endl;
+        cin >> row >> column;
+    }
+    board.at(row).at(column) = currentPlayer;
+}
+
 int main()
 {
+    vector<vector<char>> board = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+    };
+
+    char currentPlayer = 'X';
+
+    while (!gameOver(board)) {
+        printBoard(board);
+        markBoard(currentPlayer, board);
+        currentPlayer = currentPlayer == 'O' ? 'X' : 'O';
+    }
+
+    printBoard(board);
+
+    vector<int> numbersToSort = { 4, 7, 6, 9, 2 };
+    printVectorOnOneLine(numbersToSort);
+    insertionSort(numbersToSort);
+    printVectorOnOneLine(numbersToSort);
+
+
+
+
+
     vector<string> names;
     vector<int> jerseyNumbers;
 
